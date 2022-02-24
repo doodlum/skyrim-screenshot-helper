5c064c11b7aa704c217c82bf0845db08b8177e35
Tim <timpyez@gmail.com>
2022-02-24 00:13:33 +0000

    Version 1.01 changes
    - Extensive error checking and handling, as well as logging of errors
    - Check location and worldspace names up the parent trees

--- src/ScreenshotHandler.cpp
+++ src/ScreenshotHandler.cpp
@@ -19,6 +19,31 @@ std::string NumberToOrdinal(size_t number)
 	return std::to_string(number) + suffix;
 }
 
+const char* GetCurrentLocationName(RE::PlayerCharacter* PlayerCharacter) 
+{
+	auto location = PlayerCharacter->GetCurrentLocation();
+	while (location) {
+		auto locationName = location->GetName(); 
+		if (locationName && strlen(locationName) != 0) {
+			return locationName;
+		} else {
+			location = location->parentLoc;
+		}	
+	}
+
+	auto worldspace = PlayerCharacter->GetWorldspace();
+	while (worldspace) {
+		auto worldspaceName = location->GetName();
+		if (worldspaceName && strlen(worldspaceName) != 0) {
+			return worldspaceName;
+		} else {
+			worldspace = worldspace->parentWorld;
+		}
+	}
+
+	return nullptr;
+}
+
 void ScreenshotHandler::ModifyFileName(char dest[257])
 {
 	auto PlayerCharacter = RE::PlayerCharacter::GetSingleton();
@@ -27,29 +52,41 @@ void ScreenshotHandler::ModifyFileName(char dest[257])
 	if (PlayerCharacter && Calendar) {
 		std::string filename = "Screenshots\\";
 
-		filename += PlayerCharacter->GetName();
-		filename += " - ";
-		filename += PlayerCharacter->GetRace()->GetName();
-		filename += "\\";
+		if (PlayerCharacter->GetName() && PlayerCharacter->GetRace() && PlayerCharacter->GetRace()->GetName()) {
+			filename += PlayerCharacter->GetName();
+			filename += " - ";
+			filename += PlayerCharacter->GetRace()->GetName();
+			filename += "\\";
+		} else {
+			logger::error("Failed to access player information");
+		}
 
-		std::filesystem::create_directories(filename);
+		if (!std::filesystem::exists(filename))
+			std::filesystem::create_directories(filename);
 
-		auto location = PlayerCharacter->GetCurrentLocation();
+		auto currentLocationName = GetCurrentLocationName(PlayerCharacter);
+		if (currentLocationName) {
+			filename += currentLocationName;
+			filename += " - ";
+		} else {
+			logger::warn("Failed to find a location name");
+		}
 
-		filename += location ? location->GetName() : PlayerCharacter->GetWorldspace()->GetName();
-		filename += " - ";
+		if (Calendar->GetDay() && Calendar->GetMonthName() != "Bad Month" && Calendar->GetHour()) {
+			filename += NumberToOrdinal(static_cast<int>(Calendar->GetDay()));
+			filename += " of " + Calendar->GetMonthName() + ", ";
 
-		filename += NumberToOrdinal(static_cast<int>(Calendar->GetDay()));
-		filename += " of " + Calendar->GetMonthName() + ", ";
+			float time = Calendar->GetHour();
+			int   hour = static_cast<int>(time);
+			int   minute = static_cast<int>((time - hour) * 60);
 
-		float time = Calendar->GetHour();
-		int   hour = static_cast<int>(time);
-		int   minute = static_cast<int>((time - hour) * 60);
-
-		std::stringstream ss;
-		ss << hour << '.' << std::setw(2) << std::setfill('0') << minute;
-		filename += ss.str();
-		filename += hour < 12 ? "AM" : "PM";
+			std::stringstream ss;
+			ss << (hour > 12 ? hour - 12 : (hour < 1 ? hour + 12 : hour)) << '.' << std::setw(2) << std::setfill('0') << minute;
+			filename += ss.str();
+			filename += hour < 12 ? "AM" : "PM";
+		} else {
+			logger::error("Failed to access calendar");
+		}
 
 		if (std::filesystem::exists(filename + ".png")) {
 			int fixName = 1;
@@ -59,7 +96,7 @@ void ScreenshotHandler::ModifyFileName(char dest[257])
 		}
 
 		filename += ".png";
-		logger::info(FMT_STRING("filename {}"sv), filename);
+		logger::info(FMT_STRING("Save file created at {}"sv), filename);
 
 		strcpy_s(dest, 257, filename.c_str());
 	}
