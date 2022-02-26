e23919bb089d7ef6ac98f84950f620dfc502e12b
Tim <timpyez@gmail.com>
2022-02-26 23:25:25 +0000

    Wabbajack support

--- CMakeBuild.bat
+++ CMakeBuild.bat
@@ -12,8 +12,20 @@ if %ERRORLEVEL% NEQ 0 exit 1
 cmake --build build/post-ae --config Release
 if %ERRORLEVEL% NEQ 0 exit 1
 
+cmake -S . --preset=pre-ae-w --check-stamp-file "build\pre-ae-w\CMakeFiles\generate.stamp"
+if %ERRORLEVEL% NEQ 0 exit 1
+cmake --build build/pre-ae-w --config Release
+if %ERRORLEVEL% NEQ 0 exit 1
+
+cmake -S . --preset=post-ae-w --check-stamp-file "build\post-ae-w\CMakeFiles\generate.stamp"
+if %ERRORLEVEL% NEQ 0 exit 1
+cmake --build build/post-ae-w --config Release
+if %ERRORLEVEL% NEQ 0 exit 1
+
 xcopy "build\pre-ae\release\*.dll" "dist\pre-ae\SKSE\Plugins\" /I /Y
 xcopy "build\post-ae\release\*.dll" "dist\post-ae\SKSE\Plugins\" /I /Y
+xcopy "build\pre-ae-w\release\*.dll" "dist\pre-ae-w\SKSE\Plugins\" /I /Y
+xcopy "build\post-ae-w\release\*.dll" "dist\post-ae-w\SKSE\Plugins\" /I /Y
 
 xcopy "package" "dist" /I /Y /E
 
